931a6c2c50bac1377e4dc781d97da49170195556
Nithin Aditya <nithinyellam02@gmail.com>
2026-08-11 13:55:42 +0530

    Task1py

---
+++ Python/ex1.py
@@ -0,0 +1,63 @@
+
+
+
+
+# #program 1
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+city = input("Enter your city:")
+college = input("Enter your college name:")
+course = input("Enter your course name:")
+print(name, age, city, college, course)
+
+
+
+#program 2
+
+a = 20
+print(a)
+print(type(a))
+b=20.22
+print(b)
+print(type(b))
+c="Hello"
+print(c)
+print(type(c))
+d=True
+print(d)
+print(type(d))
+
+
+
+#program 3
+
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*y)
+print("Division:",x/y)
+
+
+
+#program 4
+
+m=int(input("Enter first number:"))
+n=int(input("Enter second number:"))
+print("Square of first number:",m**2)
+print("Square of second number:",n**2)
+
+
+
+#program 5
+
+name=input("Enter your name:")
+Englishmarks = float(input("Enter your English marks:"))
+Mathmarks = float(input("Enter your Math marks:"))
+Sciencemarks = float(input("Enter your Science marks:"))
+Socialmarks = float(input("Enter your Social marks:"))
+Average = (Englishmarks + Mathmarks + Sciencemarks + Socialmarks ) / 4
+print("Average marks of",name,"is:",Average)
+Totalmarks = Englishmarks + Mathmarks + Sciencemarks + Socialmarks
+print("Total marks of",name,"is:",Totalmarks)
